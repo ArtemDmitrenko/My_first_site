80400dc836d88e60cdece73093da8bb021088ea1
Artem Dmitrenko <admitrenko87@gmail.com>
2020-05-21 13:42:02 +0300

    Adding animation

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// alert ("Hello, world!!!")
+
+$(function () {
+	$(window).scroll(function() {
+	    $('.expectation__title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDown");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.plus_travel').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+    });
+    $(window).scroll(function() {
+	    $('.plus_cost').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+    });
+    $(window).scroll(function() {
+	    $('.plus__time').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+})
